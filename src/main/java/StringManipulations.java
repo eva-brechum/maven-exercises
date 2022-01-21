3c63d7e14ad88fa9eea0be2d12090312f3586860
Eva Brechum <Brechum.Eva@gmail.com>
2022-01-20 23:06:15 -0600

    completed maven-exercise

--- src/main/java/StringManipulations.java
+++ src/main/java/StringManipulations.java
@@ -3,24 +3,18 @@ import java.util.Scanner;
 
 public class StringManipulations {
     public static void main(String[] args) {
-        System.out.println("Please enter a letter or number");
+        System.out.println("Enter anything:");
         Scanner sc = new Scanner(System.in);
         String userInput = sc.nextLine();
         if (StringUtils.isNumeric(userInput)) {
-            System.out.println("You have entered a number");
-            }
-//            else if (userInput) {
-//            System.out.println("not valid");
-
-
-            System.out.println("Please enter words");
-            Scanner scanner = new Scanner(System.in);
-            String userWords = scanner.nextLine();
-            System.out.println(StringUtils.swapCase(userWords));
-
-        System.out.println("Please enter a sentence");
-//        Scanner sc = new Scanner(System.in);
-//        String userSentence = sc.new Scanner(System.in)
+            System.out.printf("%s You have entered:", userInput);
+            } else {
+            System.out.printf("%s is not a number ", userInput);
+        }
+        String flipCase = StringUtils.swapCase(userInput);
+            System.out.printf(" %s has been swapped to get %s", userInput, flipCase);
+          String reversed =StringUtils.reverse(userInput);
+        System.out.printf("in reverse:%s", userInput, reversed);
         }
     }
 
